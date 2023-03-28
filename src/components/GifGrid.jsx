77edb88568cadc6adbeaf2bede128b4aded3960a
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-03-28 13:25:05 -0500

    [+] <GifGrid>: Obtener imagenes usando el Hook useEffect

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,10 +1,13 @@
+import { useEffect } from 'react';
+
 import { getGifs } from '../helpers/get-gifs.helper';
 
 
 export const GifGrid = ({ category }) => {
-
-    // Mala Práctica: Invocar funciones dentro del cuerpo de un Functional Component
-    getGifs( category );
+    
+    useEffect(() => {
+        getGifs( category );        // Buena Práctica: Invocar funciones dentro del cuerpo de un Functional Component usando el Hook useEffect
+    }, [] );                        // No definir dependencias indica a useEffect que se ejecute solo la primera vez con la renderización del componente
 
 
     return (
