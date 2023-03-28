4eb663d7c2db9026bd114c4d5c6bee56719a9011
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-03-28 11:11:56 -0500

    [+] <App>: Crea/Muestra lista de categorias

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { useState } from 'react';
+
+
+const initialState = [ 'dogs', 'cats' ];
+
+
 const App = () => {
 
-  return (
-    <>
-      <h1>Gif Proyect App</h1>
-    </>
-  )
+    const [ categories, setCategories ] = useState( initialState );
+
+    return (
+        <>
+            {/* Title */}
+            <h1>Gif Proyect App</h1>
+
+            {/* Input */}
+
+            {/* Listado de gif */}
+            <ol>
+                {   categories.map( ( category, index ) => <li key={ index }>{ category }</li> ) }
+            </ol>
+        </>
+    )
 }
+
 
 export default App;
