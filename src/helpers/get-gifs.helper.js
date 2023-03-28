ce4c6094946c780ad09e0567937c5d3dead9730d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-03-28 13:10:49 -0500

    [/] <GifGrid>: getGifs - Fetch API Obtener imagenes de Giphy API

---
+++ src/helpers/get-gifs.helper.js
@@ -0,0 +1,20 @@
+// Buena practica: La funcion ya NO estará re-procesandose junto con el cambio del componente puesto que esta aislada del mismo
+export const getGifs = async ( search_term ) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=41kSedYo8fgY6Ofp5SH6snPYPBMrz8FL&q=${ search_term }&limit=20`;
+
+    const
+        response = await fetch( url ),
+        { data } = await response.json();
+
+    // console.log( data  );
+
+    const gifs = data.map( obj_img => ({
+        id: obj_img.id,
+        title: obj_img.title,
+        url: obj_img.images.downsized_medium.url
+    }));
+
+    console.log( gifs );
+
+    return gifs;
+}
