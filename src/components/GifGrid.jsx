60dc6284d5e5da33f52bf5a752ad0b6140843fdf
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-03-29 10:09:43 -0500

    [$] <GifGrid>: Mostrar todos los titulos de las imagenes traidas del Giphy API (async/await)

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -6,10 +6,15 @@ import { getGifs } from '../helpers/get-gifs.helper';
 export const GifGrid = ({ category }) => {
 
     const [ images, setImages ] = useState([]);
+
+
+    const getImages = async () => {
+        const newImages = await getGifs( category );
+        setImages( newImages );
+    }
     
     useEffect(() => {
-        getGifs( category )         // Buena Práctica: Invocar funciones dentro del cuerpo de un Functional Component usando el Hook useEffect
-            .then( data => setImages( data ) );
+        getImages();
     }, [] );                        // No definir dependencias indica a useEffect que se ejecute solo la primera vez con la renderización del componente
 
 
